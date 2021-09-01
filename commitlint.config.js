da3d61a70173480b5f3f947cb607e79a3ee2ca70
hejilun <hejilun@joyy.sg>
2021-09-01 20:06:27 +0800

    chore: changelog & commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,7 +8,7 @@ module.exports = {
         "feat", // 新功能 feature
         "fix", // 修复 bug
         "refactor", // 重构(既不增加新功能，也不是修复bug)
-        "doc", // 文档注释
+        "docs", // 文档注释
         "test", // 增加测试
         "chore", // 构建过程或辅助工具的变动
         "style", // 代码格式(不影响代码运行的变动)
